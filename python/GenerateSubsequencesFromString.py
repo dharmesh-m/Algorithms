1c429bbc1b6597bad79ba611836143fcc02c9140
Dharmesh Guna <dguna12@gmail.com>
2016-11-13 17:00:01 -0500

    Adding type check

--- python/GenerateSubsequencesFromString.py
+++ python/GenerateSubsequencesFromString.py
@@ -7,7 +7,14 @@ Complexity: 2 power n
 import pprint
 
 def getSubsequences(input_string):
+    if not isinstance(input_string, basestring):
+       return [[]]
+    
     slist = list(input_string)
+
+    if len(slist) == 0:
+        return [[]]
+    
     clist = [[[slist[0]]]]
     for i in range(1, len(slist)):
         nlist = list(clist[i-1])
@@ -20,6 +27,9 @@ def getSubsequences(input_string):
         clist.append(nlist)
     return clist[len(slist) - 1]
 
+pprint.pprint(getSubsequences(None))
+pprint.pprint(getSubsequences(10))
+pprint.pprint(getSubsequences('')) 
 pprint.pprint(getSubsequences('abcd'))
         
 
